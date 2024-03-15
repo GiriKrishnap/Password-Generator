26b14f5e22fc2e8a4398b8fce7c66ac48183aee4
giri <girikrishna.p6@gmail.com>
2024-03-15 11:02:21 +0530

    added firebase to gitignore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -17,4 +17,4 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
-export default app;
+export default app; 
